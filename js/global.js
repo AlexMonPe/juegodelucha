3973a10fa981ecd57b28a07188cc28123ede2d86
Alejandro <Alex_bcn10@hotmail.es>
2022-02-28 20:55:02 +0100

    Añadidas pantallas en el mismo html, comienzo de pruebas de seleccion de pjs y funct(cambiopantalla) sin funcionar

--- js/global.js
+++ js/global.js
@@ -1,73 +1,161 @@
 // -----------------------  PERSONAJE y JUGADOR ------------------------
 
 class Personaje {
-    constructor(nombre, ataque1, defensa, imagen)
-        this.nombre = nombre
-        this.vida = 100
-        this.ataque1 = ataque1
-        this.defensa = defensa
-        this.imagenpj = img
+    constructor(nombre){
+        if ( nombre == 'vegito'){
+            this.vida = 120;
+            this.ataque = 15;
+            this.defensa = 10;
+            this.imagen = './imagenes/seleccionpj/VegitoBlue.png'
+        } else if (nombre == 'celula') {
+            this.vida = 110;
+            this.ataque = 15;
+            this.defensa = 10;
+            this.imagen = './imagenes/seleccionpj/cell.png'
+        } else if (nombre == 'gohan') {
+            this.vida = 120;
+            this.ataque = 15;
+            this.defensa = 10;
+            this.imagen = './imagenes/seleccionpj/gohan.png'
+        } else if (nombre == 'zamasu'){
+            this.vida = 120;
+            this.ataque = 15;      
+            this.defensa = 10;
+            this.imagen = './imagenes/seleccionpj/ZamasuRose.png'
+        }
+    this.nombre = nombre;
+    }
+        
 }
 
-class Jugador {
-    constructor(personaje) {
-        this.personaje = personaje;
+let jugador1;
+let jugador2;
+// ------------------------------------------------CAMBIAR PANTALLA ---------------------------------- 
+const cambio = (fondo1,fondo2) => {
+    fondo1.style.display = 'none';
+    fondo2.style.display = 'flex';
+}
+
+const cambiarPantalla = () => {
+    let fondo1 = document.querySelector(".contenedor-seleccion");
+    let fondo2 = document.querySelector(".contenedor-batalla");
+    let fondo3 = document.querySelector(".contenedor-ganador");
+
+    if (fondo1.style.display == 'flex') cambio(fondo1,fondo2);
+    if (fondo2.style.display == 'flex') cambio(fondo2,fondo3);
+}
+
+const asignarPersonaje = (jugador, personaje) => {
+    jugador = new Personaje(personaje);
+    return jugador
+}
+const seleccionPersonajes = (personaje) => {
+
+    switch(personaje) {
+        case 'celula':
+            if (!jugador1) {
+                jugador1 = asignarPersonaje(jugador1, 'celula');
+            } else if (!jugador2) {
+                jugador2 = asignarPersonaje(jugador2, 'celula')
+                cambiarPantalla();
+            }
+            break;
+        case 'gohan':
+            if (!jugador1) {
+                jugador1 = asignarPersonaje(jugador1,'gohan');
+            } else if (!jugador2) {
+                jugador2 = asignarPersonaje(jugador2, 'gohan')
+                cambiarPantalla();
+            }
+            break;
+        case 'zamasu':
+            if (!jugador1) {
+                jugador1 = asignarPersonaje(jugador1,'zamasu');
+            } else if (!jugador2) {
+                jugador2 = asignarPersonaje(jugador2, 'zamasu')
+                cambiarPantalla();
+            }
+            break;
+        case 'vegito':
+            if (!jugador1) {
+                jugador1 = asignarPersonaje(jugador1,'vegito');
+            } else if (!jugador2) {
+                jugador2 = asignarPersonaje(jugador2, 'vegito')
+                cambiarPantalla();
+            }
+        break;
     }
-    Atacar(defensor, atacante) {
-        let contadorVida;
-        contadorVida = defensor.personaje.vida - atacante.personaje.ataque1 + defensor.personaje.defensa;
-        return contadorVida = defensor.personaje.vida;
-    }
+console.log(jugador1);
+console.log(jugador2);
+}
+
+// ----------------------- VOLVER A JUGAR --------------------
+const RepetirJuego = () => {
+    if (fondo3.style.display == 'flex') return cambiarPantalla(fondo3,fondo1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Atacar(defensor, atacante) {
+    //     let contadorVida;
+    //     contadorVida = defensor.personaje.vida - atacante.personaje.ataque1 + defensor.personaje.defensa;
+    //     return contadorVida = defensor.personaje.vida;
     
-}
-// ---------------- CREACION PERSONAJES----------------- 
-
-let VegitoBlue = new Personaje ("Vegito", 18, 10, `./imagenes/seleccionpj/VegitoBlue.png`);
-let Celula = new Personaje     ("Celula", 13, 12, `./imagenes/seleccionpj/cell.png`);
-let Gohan = new Personaje ("Gohan", 15, 13, `./imagenes/seleccionpj/gohan.png`);
-let ZamasuRose = new Personaje ("ZamasuRose", 20, 6, `./imagenes/seleccionpj/ZamasuRose.png`);
-
-console.log(VegitoBlue);
-console.log(Celula);
-console.log(Gohan);
-console.log(ZamasuRose);
 
 // ------------------- JUGADORES -----------------
 
-let jugador1 = new Jugador();
-let jugador2 = new Jugador();
 
-const seleccionpersonaje = (personaje) => {
 
-    switch (personaje) {
+// const seleccionpersonaje = (personaje) => {
 
-        case 'Vegito':
-            if (jugador1) {
-                jugador1 = VegitoBlue;
-            } else {
-                jugador2 = VegitoBlue;
-                Batalla();
-            } 
-        case 'Celula':
-            if (jugador1) {
-                jugador1 = Celula;
-            } else {
-                jugador2 = Celula;
-                Batalla();
-            } 
-        case 'Gohan':
-            if (jugador1) {
-                jugador1 = Gohan;
-            } else {
-                jugador2 = Gohan;
-                Batalla();
-        case 'ZamasuRose':
-            if (jugador1) {
-                jugador1 = ZamasuRose;
-            } else {
-                jugador2 = ZamasuRose;
-                Batalla();
-            }    
-    }
-}
+//     switch (personaje) {
+
+//         case 'Vegito':
+//             if (jugador1) {
+//                 jugador1 = VegitoBlue;
+//             } else {
+//                 jugador2 = VegitoBlue;
+//                 Batalla();
+//             } 
+//         case 'Celula':
+//             if (jugador1) {
+//                 jugador1 = Celula;
+//             } else {
+//                 jugador2 = Celula;
+//                 Batalla();
+//             } 
+//         case 'Gohan':
+//             if (jugador1) {
+//                 jugador1 = Gohan;
+//             } else {
+//                 jugador2 = Gohan;
+//                 Batalla();
+//         case 'ZamasuRose':
+//             if (jugador1) {
+//                 jugador1 = ZamasuRose;
+//             } else {
+//                 jugador2 = ZamasuRose;
+//                 Batalla();
+//             }    
+//     }
+// }
 
