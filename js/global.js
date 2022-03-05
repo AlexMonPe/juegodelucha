f9af4c5c28d8b55528569fc53eedeefe65186aa9
Alejandro <Alex_bcn10@hotmail.es>
2022-03-05 13:01:24 +0100

    identacion correcta y aplicar camelCase en nombres de archivos

--- js/global.js
+++ js/global.js
@@ -26,27 +26,22 @@ class Personaje {
     this.nombre = nombre;
     }
 }
-
 let jugador1;
 let jugador2;
 // ------------------------------------------------CAMBIAR PANTALLA ---------------------------------- 
+
 let personajeSeleccionado1 = document.querySelector('.personajeSeleccionado1')
 let personajeSeleccionado2 = document.querySelector('.personajeSeleccionado2')
-
-
 
 const cambio = (fondo1,fondo2) => {
     fondo1.style.display = 'none';
     fondo2.style.display = 'flex';
 }
-
 const cambiarPantalla = () => {
     let fondo1 = document.querySelector(".contenedor-seleccion");
     let fondo2 = document.querySelector(".contenedor-batalla");
     let fondo3 = document.querySelector(".contenedor-ganador");
     
-  
-
     if (fondo1.style.display == 'flex') {
         cambio(fondo1,fondo2);
         vida1.innerHTML = jugador1.vida
@@ -57,18 +52,17 @@ const cambiarPantalla = () => {
          cambio(fondo2,fondo3);
     }
 }
-
 // ------------------------- ASIGNAR PERSONAJE -------------------------
-const asignarPersonaje = (personaje) => {
-    return new Personaje(personaje);
-   
-}
+const asignarPersonaje = (personaje) => new Personaje(personaje);
+
 let contenedorpj1 = document.getElementById('personajeSeleccionado1')
 let contenedorpj2 = document.getElementById('personajeSeleccionado2')
 let bannerSeleccionado1 = document.getElementById('bannerSeleccionado1')
 let bannerSeleccionado2 = document.getElementById('bannerSeleccionado2')
-const seleccionPersonajes = (personaje) => {
 
+// ----------------------- SELECCION PERSONAJE ----------------------------------
+
+const seleccionPersonajes = (personaje) => {
     switch(personaje) {
         case 'celula':
             if (!jugador1) {
@@ -79,7 +73,7 @@ const seleccionPersonajes = (personaje) => {
                 bannerSeleccionado1.style.backgroundImage = 'url("./imagenes/personajes/caraCelula.png")'
                 bannerSeleccionado1.style.backgroundSize = 'contain'
                 bannerSeleccionado1.style.backgroundRepeat = 'no-repeat'
-                console.log(jugador1)
+                // console.log(jugador1)
             } else if (!jugador2) {
                 jugador2 = asignarPersonaje('celula')
                 contenedorpj2 .style.backgroundImage = 'url("./imagenes/personajes/celulaPelea.png")'
@@ -130,7 +124,6 @@ const seleccionPersonajes = (personaje) => {
                 bannerSeleccionado2.style.backgroundImage = 'url("./imagenes/personajes/caraZamasu.png")'
                 bannerSeleccionado2.style.backgroundSize = 'contain'
                 bannerSeleccionado2.style.backgroundRepeat = 'no-repeat'
-                bannerSeleccionado2.style.backgroundRepeat = 'no-repeat'
                 setTimeout(cambiarPantalla, 2000);
             }
             break;
@@ -172,10 +165,8 @@ const ganador = () =>{
         pjganador.style.backgroundRepeat = 'no-repeat'
     }
 }
-
 const atacar = (atacante, defensor) => {
     let vidaRestante;
-
         vidaRestante = defensor.vida - atacante.ataque1 + defensor.defensa;
         defensor.vida = vidaRestante
         console.log(defensor.vida)
@@ -183,21 +174,19 @@ const atacar = (atacante, defensor) => {
         ganador(atacante);
         cambiarPantalla();
     }
- }
+}
 
- const atacarEsp = (atacante, defensor) => {
+const atacarEsp = (atacante, defensor) => {
     let vidaRestante;
-
         vidaRestante = defensor.vida - atacante.ataque2 + defensor.defensa;
         defensor.vida = vidaRestante
         console.log(defensor.vida)
     if (defensor.vida <= 0) {
-        ganador();
+        ganador(atacante);
         cambiarPantalla();
     }
- }
-
- const combate = (jugador) => {    
+}
+const combate = (jugador) => {    
     if (jugador == 'jugador1'){
         atacar(jugador1,jugador2);
     } else {
@@ -205,8 +194,8 @@ const atacar = (atacante, defensor) => {
     }
     vida1.innerHTML = jugador1.vida
     vida2.innerHTML = jugador2.vida
- }
- const combatEsp = (jugador) => {    
+}
+const combatEsp = (jugador) => {    
     if (jugador == 'jugador1'){
         atacarEsp(jugador1,jugador2);
     } else {
@@ -214,13 +203,6 @@ const atacar = (atacante, defensor) => {
     }
     vida1.innerHTML = jugador1.vida
     vida2.innerHTML = jugador2.vida
- }
-
-// ----------------------- VOLVER A JUGAR --------------------
-// const RepetirJuego = () => {
-//     if (fondo3.style.display == 'flex') return cambiarPantalla(fondo3,fondo1);
-// }
-
-
+}
    
     
